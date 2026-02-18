e05eaa94996ee40e86fc5ef7365ec704d71fcecf
Nigmabprod <mu692018@outlook.fr>
2026-02-18 13:16:46 +0100

    Timer

--- main.py
+++ main.py
@@ -2,33 +2,14 @@
 Main program for Raspberry Pi Pico
 Simple LED blink example
 """
-#from component.HCSR04 import HCSR04
-# from machine import Pin
-# from time import sleep
 
-# def testMesure():
-#     sensor = HCSR04(trigger_pin=17, echo_pin=16)
-#     while True:
-#         print('Distance: {} mm'.format(sensor.distance_mm()))
-#         print(sensor.distance_mm())
-#         sleep(1)
-
-# def main():
-#     testMesure()
-#     led = Pin(0, Pin.OUT)
-
-#     #while True:
-#         #led.toggle()
-#         #sleep(1)  # Blink every second
-
-# if __name__ == "__main__":
-#     main()
-
-from machine import Pin
+from machine import Pin, Timer
 import time
+timer = Timer()
 trig = Pin(17, Pin.OUT)
 echo = Pin(16, Pin.IN, Pin.PULL_DOWN)
-while True:
+
+def main_loop():
     trig.value(0)
     time.sleep(0.1)
     trig.value(1)
@@ -42,4 +23,13 @@ while True:
     distance = pulse_duration * 17165 / 1000000
     distance = round(distance, 0)
     print ('Distance:',"{:.0f}".format(distance),'cm')
-    time.sleep(1)
+
+def init():
+    global timer
+    timer.init(mode=Timer.PERIODIC, period=60, callback=lambda t:main_loop())
+
+init()
+
+# Keep the program running
+while True:
+    pass
